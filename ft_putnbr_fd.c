41f4bd2b27a0bd76954f1dbb8b710234f686d5a4
Damien BAUDUIN <dbauduin@e2r7p8.42.fr>
2017-04-15 12:10:21 +0200

    putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,25 +6,25 @@
 /*   By: dbauduin <dbauduin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/14 15:12:28 by dbauduin          #+#    #+#             */
-/*   Updated: 2017/04/15 11:35:20 by dbauduin         ###   ########.fr       */
+/*   Updated: 2017/04/15 12:09:45 by dbauduin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int nb, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	unsigned int n;
+	unsigned int nb;
 
-	n = nb;
-	if (nb < 0)
+	nb = n;
+	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		n *= -1;
+		nb *= -1;
 	}
-	if (n >= 10)
+	if (nb >= 10)
 	{
-		ft_putnbr_fd((n / 10), fd);
+		ft_putnbr_fd((nb / 10), fd);
 	}
-	ft_putchar_fd(((n % 10) + '0'), fd);
+	ft_putchar_fd(((nB % 10) + '0'), fd);
 }
